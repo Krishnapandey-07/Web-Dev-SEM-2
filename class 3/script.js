f085f7af9133099f8780f40d4ca7a6bb7b328967
krishna pandey <krishnapandey@krishnas-MacBook-Air.local>
2026-02-03 12:03:53 +0530

    Classes

--- class 3/script.js
+++ class 3/script.js
@@ -1,84 +1,104 @@
-//-----Synchronous code------
+console.log("Asynchronous Programming");
 
+// --------------------synchronous code---------------
 
 // console.log("Before");
 
 // function fun(){
-//     console.log("I am fun");
+//     console.log("I am Fun");
 // }
 // fun();
 
 // console.log("After");
 
-
-//-----Asynchronous code------
+// --------------------Asynchronous code---------------
 
 // console.log("Before");
 
 // function fun(){
-//     console.log("I am fun");
+//     console.log("I am Fun");
 // }
-// setTimeout(fun,0);   //delegated to browser through Web APIs
+
+// setTimeout(fun,20);//delegated to browser through Web APIs
 
 // console.log("After");
 // console.log("After");
-// //GEC will be released and JS Callstack is free.
+// console.log("After");
+// console.log("After");
+// console.log("After");
+// console.log("After");
+// console.log("After");
+//GEC will be released and JS CallStack is free.
 
-
-//---------------async--------------
-
-
+//------------------------------async----------------
 // console.log("Before");
 
 // let flag = true;
-// function fun(){
-//     console.log("loop break");
-//     flag = false;
+
+// function fun() {
+//   //2sec
+//   console.log("loop break");
+//   flag = false;
 // }
-// setTimeout(fun, 2000);
+
+// setTimeout(fun, 2000); // runs somewhere
 
 // console.log("After");
-// console.log("Next");
+// console.log("next");
 
-// while (flag) {} //Infinite loop
+// while (flag) {} // Infinite loop
 
-//------------Quiz1---------------
-
-// console.log("Before");
+//------quiz 1
+// console.log("Before"); //1
 // const fun2 = () => {
-//     console.log("Set Timeout 1");
-//     let timeInFuture = Date.now() + 7000;
+//   console.log("Set timeout 1");//3 -> 1sec
+//   let timeInFuture = Date.now() + 7000;
 
-//     while(Date.now() < timeInFuture){}
-//     console.log("after while");
+//   while (Date.now() < timeInFuture) {}
+//   console.log("after while"); //4 ->8th second
 // };
-// const fun1 = () => console.log("Hello");
+// const fun1 = () => console.log("hello");  //5 - 8th second
+
 // setTimeout(fun1, 2000);
 // setTimeout(fun2, 1000);
 
-// console.log("After");
+// console.log("After"); //2
 
-
-//----------------Quiz2----------------
-
-// console.log("Start");
+//----------------------quiz2----------------------
+// console.log("Start"); //1 t=0
 // setTimeout(() => {
-//     console.log("First Timeout(1s)");
-//     setTimeout(() => {
-//         console.log("Inner Timeout(0.5s)");
-//     }, 500);
+//   console.log("First Timeout (1s)"); //6 t=1
+
+//   setTimeout(() => {
+//     console.log("Inner Timeout (0.5s)"); //7 t=1.5
+//   }, 500);
 // }, 1000);
+
 // setTimeout(() => {
-//     console.log("Second timeout(0s)");
+//   console.log("Second Timeout (0s)"); //4 t=0
 // }, 0);
-// function runTask(){
-//     console.log("Inside run Task");
-//     setTimeout(() => {
-//         console.log("Timeout inside runTask(300ms)");
-//     }, 300);
-// function innerTask(){
-//     console.log("Inside InnerTask");
-// }
-// innerTask();
+
+// function runTask() {
+//   console.log("Inside runTask"); //2 t=0
+
+//   setTimeout(() => {
+//     console.log("Timeout inside runTask (300ms)"); //5 t=0.3
+//   }, 300);
+
+//   function innerTask() {
+//     console.log("Inside innerTask"); //3 t=0
+//   }
+
+//   innerTask();
 // }
 // runTask();
+
+
+// ------setInterval------
+// let i = 0;
+// let id = setInterval(() => {
+//   console.log("I am inside interval ", ++i);
+//   if(i==3){
+//     clearInterval(id)
+//   }
+// }, 1000);
--- class 5/script.js
+++ class 5/script.js
@@ -1,45 +1,105 @@
 // const cart = ["shirt", "trousers", "shoes", "shades"];
 
-// const promise = createOrder(cart); //async function -- will get orderID
+// const promise = createOrder(cart); // An async function -- will get orderId
 
-// //print
+// //Print PROMISE
+// console.log(promise);
 
-// //consume
-
-// promise.then(function(orderId){
-//     //promise is fulfilled
-//     console.log(orderId);
+// //Consume
+// promise.then(function (orderId) {
+//     console.log("inside then", promise);
+//   //This runs when Promise is resolved
+//   console.log(orderId);
+// }).catch(function(err){
+//     console.log(err.message)
+//     alert("error",err.message)
 // })
 
-// //Creation of promise / backend side / producer side
+// //Creation of promise/ backend side / producer side
 
-// function createOrder(cart){
-//     const pr = new Promise(function(resolve,reject){
-//         //CreateOrder
-//         //Validate the cart (check in stock)
-//         //return orderId
+// function createOrder(cart) {
 
-//         // Failed-first approach
-
-//         if(!validateCart(cart)){
-//             const err = new Error("Cart is invalid");
-//             reject(err)
-//         }
-
-//         //logic for createOreder
-
-//         const orderId = '1101'
-
-//         if(orderId){
-//             setTimeout(function(){
-//                 resolve(orderId);
-//             },2000)
-//         }
-//     });
-
-//     return pr;
+// function validateCart() {
+//   return false;
 // }
 
-// function validateCart(){
-//     return true;
+//   const pr = new Promise(function (resolve, reject) {
+//     //createOrder
+//     //validate the cart (check in stock)
+//     //return orderId
+
+//     //Fail-first approach
+//     if (!validateCart(cart)) {
+//       setTimeout(function () {
+//         // mimicking async BE jobs.
+//         const err = new Error("Cart is invalid");
+//         reject(err);
+//       }, 2000);
+//     }
+
+//     // logic for createOrder
+
+//     const orderId = "11010";
+
+//     if (orderId) {
+//       setTimeout(function () {
+//         // mimicking BE jobs.
+//         resolve(orderId);
+//       }, 5000);
+//     }
+//   });
+
+//   return pr;
 // }
+
+//======================= Promise chaining=======================
+const cart = ["shirt", "trousers", "shoes", "shades"];
+
+createOrder(cart)
+  .then(function (orderId) {
+    console.log(orderId);
+    return orderId;
+  })
+  .then(function (orderId) {
+    return proceedToPayment(orderId);
+  }).then((paymentInfo)=> {
+        console.log(paymentInfo)
+    })
+  .catch(function (err) {
+    console.log(err.message);
+  }).then(function(){
+    console.log("I will be definitely called !!")
+  });
+
+// Creation / Backend side /Promises are produced
+function createOrder(cart) {
+  const pr = new Promise(function (resolve, reject) {
+    //createOrder
+    //validateCart
+    //return orderId
+
+    //Fail-first approach
+    if (!validateCart(cart)) {
+      const err = new Error("Cart is invalid");
+      reject(err);
+    }
+
+    // logic for createOrder
+
+    const orderId = "11011";
+    if (orderId) {
+      setTimeout(() => resolve(orderId), 4000);
+    }
+  });
+  return pr;
+}
+
+function proceedToPayment(orderId) {
+  return new Promise(function (resolve, reject) {
+    resolve("Payment Successful");
+  });
+}
+
+function validateCart(cart) {
+  return false;
+}
